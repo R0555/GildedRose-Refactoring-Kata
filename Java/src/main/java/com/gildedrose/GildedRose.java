ab6426697ac039a208b2f21ad316ca733ef7ca33
Ross Boulton <rossdboulton@gmail.com>
2025-11-10 16:36:25 +0100

    Extract update logic for each item type into a separate function

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -16,30 +16,47 @@ class GildedRose {
     private void updateItem(Item item) {
         switch (item.name) {
             case "Sulfuras, Hand of Ragnaros":
+                updateSulfuras(item);
                 break;
             case "Aged Brie":
-                item.sellIn--;
-                item.quality = item.sellIn < 0 ? Math.min(item.quality + 2, 50) : Math.min(item.quality + 1, 50);
+                updateAgedBrie(item);
                 break;
             case "Backstage passes to a TAFKAL80ETC concert":
-                item.sellIn--;
-                if (item.sellIn < 0) {
-                    item.quality = 0;
-                } else if (item.sellIn < 5) {
-                    item.quality = Math.min(item.quality + 3, 50);
-                } else if (item.sellIn < 10) {
-                    item.quality = Math.min(item.quality + 2, 50);
-                } else {
-                    item.quality = Math.min(item.quality + 1, 50);
-                }
+                updateBackstagePass(item);
                 break;
             default:
-                item.sellIn--;
-                item.quality = Math.max(item.quality - 1, 0);
-                if (item.sellIn < 0) {
-                    item.quality = Math.max(item.quality - 1, 0);
-                }
+                updateDefault(item);
                 break;
+        }
+    }
+
+    private void updateSulfuras(Item item) {
+        return;
+    }
+
+    private void updateAgedBrie(Item item) {
+        item.sellIn--;
+        item.quality = item.sellIn < 0 ? Math.min(item.quality + 2, 50) : Math.min(item.quality + 1, 50);
+    }
+
+    private void updateBackstagePass(Item item) {
+        item.sellIn--;
+        if (item.sellIn < 0) {
+            item.quality = 0;
+        } else if (item.sellIn < 5) {
+            item.quality = Math.min(item.quality + 3, 50);
+        } else if (item.sellIn < 10) {
+            item.quality = Math.min(item.quality + 2, 50);
+        } else {
+            item.quality = Math.min(item.quality + 1, 50);
+        }
+    }
+
+    private void updateDefault(Item item) {
+        item.sellIn--;
+        item.quality = Math.max(item.quality - 1, 0);
+        if (item.sellIn < 0) {
+            item.quality = Math.max(item.quality - 1, 0);
         }
     }
 }
